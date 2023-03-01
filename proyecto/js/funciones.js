83928539d8e136bcdadb51455496d79ffd4ea152
Dana <danairamain@gmail.com>
2023-03-01 20:25:03 -0300

    primer commit

---
+++ proyecto/js/funciones.js
@@ -0,0 +1,10 @@
+
+document.getElementById('icon-menu').addEventListener(click, function() {
+    document.getElementsByClassName('text-bar').style.display="none"
+})
+
+
+
+document.getElementById('boton-hola').addEventListener('click', function(){
+    document.getElementById('formulario-contacto').style.display ='none';
+})
