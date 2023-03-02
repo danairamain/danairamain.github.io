3778e6ad867fa1a8101c5fc3a601c85d7e234f24
danairamain <126197703+danairamain@users.noreply.github.com>
2023-03-01 23:33:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const toggle = document.querySelector('#toggle');
+const sidebar = document.querySelector('#side-bar');
+
+toggle.addEventListener('click', function(){
+    sidebar.style.widht='170px'
+})
