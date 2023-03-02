37ce5a5591cc5517f630256dfb183b0a1ceeaa06
danairamain <126197703+danairamain@users.noreply.github.com>
2023-03-01 23:37:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const toggle = document.querySelector('#toggle');
+const toggle = document.querySelector('#boton-menu');
 const sidebar = document.querySelector('#side-bar');
 
 toggle.addEventListener('click', function(){
